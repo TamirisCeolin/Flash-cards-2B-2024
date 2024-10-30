66844a8f8d769c97913548980bb36a2ee569b333
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-10-30 10:20:37 -0300

    30/10

--- aap.js
+++ aap.js
@@ -22,12 +22,7 @@ function viraCartao(){
     cartao.classList.toggle('active', respostaEstaVisivel);
 }
 
-
-
-
-
-
-
+cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
