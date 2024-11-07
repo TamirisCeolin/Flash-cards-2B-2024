87506e909b3a68ff569484fcc947abd602bf33a1
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-07 10:54:43 -0300

    07/11

--- aap.js
+++ aap.js
@@ -15,14 +15,14 @@ function criaCartao(categoria, pergunta, resposta) {
                 </div>
     `
 
-    let respostaEstaVisivel = false;
+    let respostaEstaVisivel = false
 
     function viraCartao() {
-        respostaEstaVisivel =!respostaEstaVisivel;
-        cartao.classList.toggle('active', respostaEstaVisivel);
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
+    cartao.addEventListener('click', viraCartao)
 
-    cartao.addEventListener('click', viraCartao);
 
-    container.appendChild(cartao);
+    container.appendChild(cartao)
 }
